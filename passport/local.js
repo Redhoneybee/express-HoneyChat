192e376a7447730900c16af5fd0f6df92f7af8ea
honeybee <flack319@gmail.com>
2020-04-04 18:46:03 +0900

    local passport

--- passport/local.js
+++ passport/local.js
@@ -11,12 +11,9 @@ module.exports = (passport) => {
   }, async (id, password, done) =>{
     try{
       const exId = await User.findOne( { id } );
-      console.log(exId);
       if(exId){
         // user is
         const result = await bcrypt.compare(password, exId.password);
-        console.log(password);
-        console.log(result);
         if(result) { done(null, exId); }
         else { done(null, false, { message : 'no matched password' }); }
       }else{
